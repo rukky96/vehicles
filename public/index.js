1cfebef62ba2dcf434af1189089bcacc420094dc
Akproko Kelvin <akprokokelvin@gmail.com>
2025-05-06 13:52:13 +0100

    first commit

---
+++ public/index.js
@@ -0,0 +1,88 @@
+let cars = [];
+let currentId = null;
+
+async function fetchData() {
+    const tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = "";
+    try {
+        const response = await fetch('http://localhost:3333/api/cars');
+        const data = await response.json();
+        cars = data;
+        cars.forEach((car, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${car.brand}</td>
+        <td>${car.model}</td>
+        <td>${car.year}</td>
+        <td>${car.color}</td>
+        <td><button class="edit-button" style="margin-right: 5px;">Edit</button><button class="delete-button">Delete</button></td>
+        `
+        row.querySelector('.edit-button').addEventListener("click", () => {
+            const brand = document.getElementById("brand");
+            const model = document.getElementById("model");
+            const year = document.getElementById("year");
+            const color = document.getElementById("color");
+
+            document.getElementById("edit-form").style.display = "flex";
+
+            brand.value = `${car.brand}`;
+            model.value = `${car.model}`;
+            year.value = `${car.year}`;
+            color.value = `${car.color}`;
+            currentId = car.id;
+
+        })
+
+        tableBody.appendChild(row);
+    })
+        console.log(cars);
+    } catch (error){
+        console.log(error);
+    }
+
+    
+}
+
+document.getElementById("load-button").addEventListener("click", (e)=>{
+    fetchData();
+    
+})
+
+document.getElementById("cancel-button").addEventListener("click", (e) =>{
+    document.getElementById("edit-form").style.display = "none";
+})
+
+document.getElementById("submit-button").addEventListener("click", async (e) => {
+            const brand = document.getElementById("brand").value;
+            const model = document.getElementById("model").value;
+            const year = document.getElementById("year").value;
+            const color = document.getElementById("color").value;
+
+
+    try {
+        const response = await fetch(`http://localhost:3333/api/cars/${currentId}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    brand: brand,
+                    model: model,
+                    year: year,
+                    color: color,
+                }),
+                
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+        document.getElementById("edit-form").style.display = "none";
+        fetchData();
+    } catch(error){
+        console.log(error);
+    }
+})
+
+
---
+++ server.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const {Pool} = require("pg");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploads")));
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+});
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        const {first_name, last_name} = req.body;
+        const extension = path.extname(file.originalname);
+
+        cb(null, `${first_name.trim().toLowerCase()}-${last_name.trim().toLowerCase}${extension}`);
+    }
+});
+
+const upload = multer({storage})
+
+app.post('/api/uploads', upload.single('image'), (req, res) => {
+    const {first_name, last_name} = req.body;
+
+    if (!req.file) {
+        res.status(400).json({message: "Image is required"})
+    } else if (!first_name || !last_name){
+        res.status(400).json({message: "All fields are required"});
+    } else {
+        req.file.filename = `${first_name.toLowerCase()}-${last_name.toLowerCase()}`;
+        res.status(201).json({
+            message: "Image successfully uploaded",
+            first_name,
+            last_name,
+            filePath: `http://localhost:3333/uploads/${req.file.filename}`
+        })
+    }
+})
+
+app.get('/api/cars', async (req, res) =>  {
+    try {
+        const query = `SELECT * FROM cars ORDER BY id;`
+        const connection = await pool.query(query);
+        const results = connection.rows
+        res.status(200).json(results);
+        console.log('results')
+    } catch(error){
+        console.log(error)
+        res.status(500).json({
+            message: 'There was an internal server error'
+        })
+    }
+})
+
+app.get('/api/cars/:id', async (req, res)=> {
+    const id = req.params.id;
+    const query = `SELECT * FROM cars WHERE id = ${id};`
+    try {
+        const connection = await pool.query(query);
+        const results = connection.rows
+        res.status(200).json(results);
+        console.log('results')
+    } catch(error){
+        console.log(error)
+        res.status(500).json({
+            message: 'There was an internal server error'
+        })
+    }
+})
+
+app.put('/api/cars/:id', async (req, res)=> {
+    const id = req.params.id;
+    const {brand, model, year, color} = req.body;
+    const query = `
+    UPDATE cars 
+    SET brand = $1, model = $2, year = $3, color = $4
+    WHERE id = ${id}
+    RETURNING *;`
+
+    if (!brand || !model || !year || !color) {
+        res.status(0).json({message: 'Some fields are missing'})
+    } else {
+        try {
+            const values = [brand, model, year, color];
+            const connection = await pool.query(query, values);
+            const results = connection.rows
+            res.status(200).json(results);
+            console.log('results')
+        } catch(error){
+            console.log(error)
+            res.status(500).json({
+                message: 'There was an internal server error'
+            })
+        }
+    }
+    
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.listen(3333, () => {
+    console.log("Server is running");
+})
